d704c72bf636521f0073de693f807c47b659863a
Eric Lofgren <Eric.Lofgren@wsu.edu>
2018-04-23 21:59:46 -0700

    Integrating code
    Improved code as developed in class

--- Code/GIllespie_MultipleRuns.r
+++ Code/GIllespie_MultipleRuns.r
@@ -5,8 +5,9 @@ nu <- matrix(c(-1,0,
                +1,-1,
                0, +1),nrow=3,byrow=T)
 
-out <- lapply(X=1:10,FUN=function(x) ssa(x0, a, nu, tf=100)$data)
+nruns <- 100
+time <- 100
+out <- lapply(X=1:nruns,FUN=function(x) ssa(x0, a, nu, tf=time)$data)
 
-plot(out[[1]][,1],out[[1]][,2], ylim=c(0,500), col="red3", type="l")
-nruns <- 10
-for (i in 2:nruns){lines(out[[i]][,1],out[[i]][,2])}
+plot(out[[1]][,1],out[[1]][,3], ylim=c(0,500),xlim=c(0,time),col="black", type="l")
+for (i in 2:nruns){lines(out[[i]][,1],out[[i]][,3])}
